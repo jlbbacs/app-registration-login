fc2f3f21aaf5617d592ba0f07fd4253f32bd1dc1
josephlesterbacs <106429121+jlbbacs@users.noreply.github.com>
2026-03-02 23:30:03 +0800

    fixing tailwind

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './index.css' // <--- Essential for the production build
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
